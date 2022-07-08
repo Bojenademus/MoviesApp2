1277c06096cab2d24122c5d64d9036395041b197
Bozhena Demus <20086514@mail.wit.ie>
2022-07-08 20:29:35 +0100

    Home page components

---
+++ src/stories/movieCard.stories.js
@@ -0,0 +1,26 @@
+import React from "react";
+import MovieCard from "../components/movieCard";
+import SampleMovie from "./sampleData";
+
+export default {
+  title: "Home Page/MovieCard",
+  component: MovieCard,
+};
+
+export const Basic = () => {
+  return (
+    <MovieCard
+      movie={SampleMovie}
+    />
+  );
+};
+Basic.storyName = "Default";
+
+export const Exceptional = () => {
+  const sampleNoPoster = { ...SampleMovie, poster_path: undefined };
+  return (
+    <MovieCard
+      movie={sampleNoPoster}
+    />
+  );
+};
