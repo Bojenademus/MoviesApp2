942e7a77da4aae7c4368a87d2a904776f47c83a9
Bozhena Demus <20086514@mail.wit.ie>
2022-07-16 15:22:03 +0100

    Fix upcoming movies page.

--- src/pages/upcomingMovies.js
+++ src/pages/upcomingMovies.js
@@ -1,34 +1,25 @@
 import React, { useState, useEffect } from "react";
 import PageTemplate from '../components/templateMovieListPage'
 import { getUpcomingMovies } from "../api/tmdb-api";
+import AddToFavouritesIcon from '../components/cardIcons/addToFavourites'
+
 
 const UpcomingMovies = (props) => {
   const [movies, setMovies] = useState([]);
-  const mustwatchMovies = movies.filter(m => m.mustwatch)
-  localStorage.setItem('mustWatchMovies', JSON.stringify(mustwatchMovies))
-
-  // todo: fix add to must watch functionality
-  /*const addToMustwatch = (movieId) => {
-    const updatedMovies = movies.map((m) =>
-      m.id === movieId ? { ...m, mustwatch: true } : m
-    );
-    setMovies(updatedMovies);
-  };*/
-  
- const addToMustwatch = null;
 
   useEffect(() => {
     getUpcomingMovies().then(movies => {
       setMovies(movies);
     });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <PageTemplate
       title='Upcoming Movies'
       movies={movies}
-      selectMustwatch={addToMustwatch}
+      action={(movie) => {
+        return <AddToFavouritesIcon movie={movie} />
+      }}
     />
   );
 };
